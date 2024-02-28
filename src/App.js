96d12dd8e8154d5e42513ca1b11ae9c88b8939df
larinpaul <www.larinpavel@mail.ru>
2024-02-28 13:13:55 +0500

    const title = 'my  text' <h1>{ titlte }<h1/>  ... npm start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to the new blog, fellow kids!';
+
   return (
     <div className="App">
-      <h1>Greetings, fellow kids!</h1>      
+      <h1>{ title }</h1>      
     </div>
   );
 }
